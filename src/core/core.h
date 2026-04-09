8de39646adf308d2095660e1ccdf2717392bc326
Michal Brodziak <niesmialymichal@gmail.com>
2026-04-09 23:13:30 +0200

    wire up

--- src/core/core.h
+++ src/core/core.h
@@ -1,12 +1,15 @@
 #ifndef VISITOR_CORE_H
 #define VISITOR_CORE_H
 
+#include "pane.h"
+#include <tuple>
+
 namespace visitor
 {
     // Orchestrates filesystem, manages other functionalities,
     // presistence, scripting etc.
     class Core {
-
+        std::tuple<Pane, Pane> panes;
     };
 }
 
---
+++ src/core/pane.h
@@ -0,0 +1,12 @@
+#ifndef VISITOR_PANE_H
+#define VISITOR_PANE_H
+
+#include <string>
+namespace visitor
+{
+    struct Pane {
+        std::string current_path;
+    };
+}
+
+#endif
