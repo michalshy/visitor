f51e436c707e5bde3a58eb910144145640c9e3a9
Michal Brodziak <niesmialymichal@gmail.com>
2026-07-14 21:53:49 +0200

    prepare action scaffold

--- libvisitor/src/actions.rs
+++ libvisitor/src/actions.rs
@@ -1,1 +1,41 @@
 use crate::VEntry;
+
+pub fn act_create_file() {
+
+}
+
+pub fn act_create_dir() {
+
+}
+
+pub fn act_delete() {
+
+}
+
+pub fn act_copy() {
+
+}
+
+pub fn act_cut() {
+    
+}
+
+pub fn act_paste() {
+
+}
+
+pub fn act_rename() {
+
+}
+
+pub fn act_create_symlink() {
+
+}
+
+pub fn act_compress() {
+
+}
+
+pub fn act_extract() {
+    
+}
